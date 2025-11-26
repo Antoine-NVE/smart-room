a4b389169333964216e0cd6b9dea17715179c689
Antoine-NVE <antoinenavette@gmail.com>
2025-11-26 15:15:13 +0100

    style: format

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,15 @@
-import {createServer} from "./presentation/server";
+import { createServer } from './presentation/server';
 
 const start = () => {
     const app = createServer();
     const server = app.listen(3000);
     server.on('listening', () => {
-        console.log('Server started')
+        console.log('Server started');
     });
     server.on('error', (err: unknown) => {
-        console.error('Server startup failed:', err)
+        console.error('Server startup failed:', err);
         process.exit(1);
     });
-}
+};
 
 start();
