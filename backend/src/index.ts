fe3f82b3432798ec19a875c3a02e1b58a740884e
Antoine-NVE <antoinenavette@gmail.com>
2025-11-26 10:16:24 +0100

    chore: docker

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import {createServer} from "./presentation/server";
+
+const start = () => {
+    const app = createServer();
+    const server = app.listen(3000);
+    server.on('listening', () => {
+        console.log('Server started')
+    });
+    server.on('error', (err: unknown) => {
+        console.error('Server startup failed: ', err)
+    });
+}
+
+start();
