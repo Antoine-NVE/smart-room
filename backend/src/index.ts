b66e42261c8d76ec74e43725364baa3be8ff37db
Antoine-NVE <antoinenavette@gmail.com>
2025-11-27 09:56:12 +0100

    chore: create base container

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,14 @@
 import { createServer } from './presentation/server';
 import { loadEnv } from './infrastructure/env';
+import { buildContainer } from './infrastructure/container';
 
 const start = () => {
     const env = loadEnv();
     console.log('Environment validated');
+
+    // TODO: connect to DB
+
+    const container = buildContainer();
 
     const app = createServer();
     const server = app.listen(3000);
