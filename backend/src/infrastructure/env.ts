88c09907f39ec0565fbd44f3f4a9f418e668caad
Antoine-NVE <antoinenavette@gmail.com>
2025-11-27 10:46:28 +0100

    chore(backend): add db credentials to environment

--- backend/src/infrastructure/env.ts
+++ backend/src/infrastructure/env.ts
@@ -3,13 +3,19 @@ import { z } from 'zod';
 const envSchema = z.object({
     NODE_ENV: z.enum(['development', 'production']),
     ALLOWED_ORIGINS: z.array(z.url()),
+    DB_USER: z.string(),
+    DB_PASSWORD: z.string(),
 });
 
 export type Env = ReturnType<typeof loadEnv>;
 
 export const loadEnv = () => {
+    const { NODE_ENV, ALLOWED_ORIGINS, DB_USER, DB_PASSWORD } = process.env;
+
     return envSchema.parse({
-        NODE_ENV: process.env.NODE_ENV,
-        ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS?.split(','),
+        NODE_ENV,
+        ALLOWED_ORIGINS: ALLOWED_ORIGINS?.split(','),
+        DB_USER,
+        DB_PASSWORD,
     });
 };
